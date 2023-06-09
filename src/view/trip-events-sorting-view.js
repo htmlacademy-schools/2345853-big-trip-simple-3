5688fcfc5a99979742d520e95e811bdf3f45e60e
ericsupersonic <happyokynekgmail.com>
2023-06-10 00:16:47 +0300

    module5-task2

--- src/view/trip-events-sorting-view.js
+++ src/view/trip-events-sorting-view.js
@@ -1,8 +1,19 @@
 import AbstractView from '../framework/view/abstract-view';
-const createTripSortingBlock = (sort) => `<div class="trip-sort__item  trip-sort__item--${sort.toLowerCase()}">
-      <input id="sort-${sort.toLowerCase()}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${sort.toLowerCase()}" checked>
-      <label class="trip-sort__btn" for="sort-${sort.toLowerCase()}">${sort}</label>
-    </div>`;
+import {SortType} from '../utils/trip-points';
+
+const createTripSortingBlock = (name) => (
+  `
+    <div class="trip-sort__item  trip-sort__item--${name.toLowerCase()}">
+      <input
+        id="sort-${name.toLowerCase()}"
+        class="trip-sort__input visually-hidden"
+        type="radio" name="trip-sort"
+        value="sort-${name.toLowerCase()}"
+        ${name === SortType.DAY ? 'checked' : ''}
+      >
+      <label class="trip-sort__btn" for="sort-${name.toLowerCase()}" data-sort-type="${name}">${name}</label>
+    </div>`
+);
 const createTripEventsSortingTemplate = (sorts) => `
   <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
     ${sorts.map((sort) => createTripSortingBlock(sort)).join('')}
@@ -15,6 +26,19 @@ class TripEventsSortingView extends AbstractView {
     super();
     this.#sorts = sorts;
   }
+
+  setSortTypeChangeHandler = (callback) => {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  };
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'LABEL') {
+      return;
+    }
+
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  };
 
   get template() {
     return createTripEventsSortingTemplate(this.#sorts);
